c4d5fe63e66de03ea67e98e780e87d78c5516991
zhangyingwei <723129bao>
2016-06-12 09:09:20 +0800

    修改了接口文件名称

--- src/main/java/com/zhangyw/kafka/producer/server/impl/KPServerImpl.java
+++
@@ -1,8 +0,0 @@
-package com.zhangyw.kafka.producer.server.impl;
-
-import com.zhangyw.kafka.producer.exception.KPSException;
-
-public interface KPServerImpl {
-	public void start() throws KPSException;
-	public void send(String topic,String message) throws KPSException;
-}
